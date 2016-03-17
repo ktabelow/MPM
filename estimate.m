f0c6fe8aada9a870b2998919ddf96a99cc36a1c3
Chiara D'Alonzo <dalonzo@lin154.wias-berlin.de>
2016-03-17 10:18:47 +0100

    eliminated repelem (function only from 2015a) replaced with kron

--- estimate.m
+++ estimate.m
@@ -2,15 +2,15 @@
 % has to run for all i in sdim(3) to get the complete model
 %
 % =========================================================================
-% 2015/09/30
+% 
+% last modified 2016/03/17
 %
-% matlab Implementation of the R function estimateESTATICS.r in package qMRI
-% written by K. Tabelow and J. Polzehl (?)
+% matlab Implementation written by C. D'Alonzo 
+% of the R function estimateESTATICS.r in package qMRI written by K. Tabelow
 %
-% from Lars Ruthotto 
-% functions loadImageSPM, GaussNewton, FLASHobjFctn, FLASHobjFctn2, 
-% Armijo  all written by Lars Ruthotto
-% http://www.mathcs.emory.edu/~lruthot/
+% 
+% functions loadImageSPM, GaussNewton, FLASHObjectiveFunction, 
+% written by Lars Ruthotto http://www.mathcs.emory.edu/~lruthot/
 %
 % 
 %
@@ -79,7 +79,7 @@ for k=1:length(pdFiles),
     [PD(k,:,:,:),omega] = loadImageSPM(fullfile(pdFiles{k}),'slices',slices);
 end
 
-TE = TE./TEScale; %[t1TE(:)./100; mtTE(:)./100; pdTE(:)./100];
+TE = TE./TEScale; 
 %m0 = [1000/DataScale; 1000/DataScale;1000/DataScale; 0.05*TEScale];
 if ~isempty(mtFiles)
     m0 = [1000; 1000;1000; 0.05*TEScale];
@@ -90,16 +90,14 @@ end
 %% perform optimization
 
 
-    % the commented part does the optimisation only on the elements in mask
-    % but it ist too slow...
-    %T1t = reshape(T1(:,logical(maski)), size(T1,1),[]);
-    %MTt = reshape(MT(:,logical(maski)), size(MT,1),[]);
-    %PDt = reshape(PD(:,logical(maski)), size(PD,1),[]);
-    T1t = reshape(T1, size(T1,1),[]); %
+    % the optimisation only on the elements in mask was too slow and was
+    % eliminated
     
-    PDt = reshape(PD, size(PD,1),[]); %
+    T1t = reshape(T1, size(T1,1),[]); 
+    
+    PDt = reshape(PD, size(PD,1),[]); 
     if ~isempty(mtFiles)
-        MTt = reshape(MT, size(MT,1),[]); %
+        MTt = reshape(MT, size(MT,1),[]); 
         data = [T1t./DataScale; MTt./DataScale; PDt./DataScale];
 
 
@@ -113,13 +111,7 @@ end
         mi1 = ProjGaussNewton(fctn,m0big,'tolJ',tolerance,'tolG',tolerance,'tolU',tolerance,'lower',lower,'maxIter',10);
         
 
-        %maski = reshape(maski,numel(maski),[]);
-        %maskbig = repelem(maski,4);
-        %m1big = zeros(4*prod(m),1);
-
-        % fill in information and set correct dimension
-        %m1big(logical(maskbig),1) = mi1(:,1);
-        %res.coeff=m1big; %
+        
 
         res.coeff=mi1;
 
@@ -128,19 +120,12 @@ end
         [~,para,dD,~,Hstar] = fctn(mi1); % ~ is for H, Hstar contains only J'*J
         sig2 = para.Dcs/(size(TE,1)-numel(m0));
 
-
-        %iind = 1: length(maski);
-        %iind = iind(logical(maski));
-        %dia = sparse(iind,iind,ones(length(iind),1),sdim(1)*sdim(2),sdim(1)*sdim(2)); %,sdim(1)*sdim(2),sdim(1)*sdim(2)
-        %big_indexing = kron(dia, [1 1 1 1 ; 1 1 1 1; 1 1 1 1 ; 1 1 1 1]);
-        %small_indexing = kron(speye(numel(iind)),[1 1 1 1 ; 1 1 1 1; 1 1 1 1 ; 1 1 1 1] );
-        %small_invCov = H./sig2;
-        %res.invCov = spalloc(4*sdim(1)*sdim(2),4*sdim(1)*sdim(2),numel(small_invCov));
-        %res.invCov(logical(big_indexing))=small_invCov(logical(small_indexing));
         res.invCov=Hstar;
-        res.sig2 = repelem(sig2,nv*nv);
-        %res.invCov=J'*J./sig2;
-        %res.invCov=H/sig2; %res.invCov=H./sig2;
+        %res.sig2 = repelem(sig2,nv*nv);% unluckily is repelem not
+        %available for matlab version older then 2015a
+       
+        res.sig2 = kron(sig2,ones(1,nv*nv));
+        
         res.para=para;
         res.dD=dD;
         res.omega=omega;
@@ -157,13 +142,7 @@ end
         % mi1 = GaussNewton(fctn,m0big);
         mi1 = ProjGaussNewton(fctn,m0big,'tolJ',tolerance,'tolG',tolerance,'tolU',tolerance,'lower',lower,'maxIter',10);
 
-        %maski = reshape(maski,numel(maski),[]);
-        %maskbig = repelem(maski,4);
-        %m1big = zeros(4*prod(m),1);
-
-        % fill in information and set correct dimension
-        %m1big(logical(maskbig),1) = mi1(:,1);
-        %res.coeff=m1big; %
+        
 
         res.coeff=mi1;
 
@@ -173,17 +152,12 @@ end
         sig2 = para.Dcs/(size(TE,1)-numel(m0));
 
 
-        %iind = 1: length(maski);
-        %iind = iind(logical(maski));
-        %dia = sparse(iind,iind,ones(length(iind),1),sdim(1)*sdim(2),sdim(1)*sdim(2)); %,sdim(1)*sdim(2),sdim(1)*sdim(2)
-        %big_indexing = kron(dia, [1 1 1 1 ; 1 1 1 1; 1 1 1 1 ; 1 1 1 1]);
-        %small_indexing = kron(speye(numel(iind)),[1 1 1 1 ; 1 1 1 1; 1 1 1 1 ; 1 1 1 1] );
-        %small_invCov = H./sig2;
-        %res.invCov = spalloc(4*sdim(1)*sdim(2),4*sdim(1)*sdim(2),numel(small_invCov));
-        %res.invCov(logical(big_indexing))=small_invCov(logical(small_indexing));
 
         res.invCov=Hstar; %
-        res.sig2 = repelem(sig2,nv*nv);
+        %res.sig2 = repelem(sig2,nv*nv); % unluckily is repelem not
+        %available for matlab version older then 2015a
+        
+        res.sig2 = kron(sig2,ones(1,nv*nv));
         res.para=para;
         res.dD=dD;
         res.omega=omega;
