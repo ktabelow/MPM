760ed546bd381dacc9e9fa91112ad242328de4f5
Chiara D'Alonzo <dalonzo@lin154.wias-berlin.de>
2017-06-21 15:28:25 +0200

    bugfix: now set kstar in smoothing

--- mpmESTATICS.m
+++ mpmESTATICS.m
@@ -414,7 +414,7 @@ function [] = mpmESTATICS(job)
 %             else
 %                       
 %             end
-            modelMPMs_mask = smoothESTATICSmask(modelMPM, 'verbose', false, 'wghts', wghts, 'lambda',job.lambda); 
+            modelMPMs_mask = smoothESTATICSmask(modelMPM, 'verbose', false, 'wghts', wghts, 'kstar',job.kstar,'lambda',job.lambda); 
             qiSnew = calculateQI(modelMPMs_mask, 'TR2',job.tr2,'b1File',job.b1File , 'verbose', false);
         else
             qiSnew = calculateQI(modelMPM, 'TR2',job.tr2,'b1File',job.b1File , 'verbose', false);            
