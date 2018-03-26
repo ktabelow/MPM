40d1204fa702739d248143d87b08f1be59f8f86d
Karsten Tabelow <karsten.tabelow@wias-berlin.de>
2018-03-26 13:04:43 +0200

    patch-wise AWS compiles with R2017b and Xcode 9

--- make_MPM.m
+++ make_MPM.m
@@ -1,6 +1,7 @@
 % to compile the fortran files
 
 mex gethani.F cgethani.F
-mex FFLAGS='$FFLAGS -fopenmp' -largeArrayDims vaws2.F cvaws2.F;
+mex FFLAGS='$FFLAGS' -largeArrayDims vaws2.F cvaws2.F;
+mex FFLAGS='$FFLAGS' -largeArrayDims vpaws2.F cpvaws2.F;
 
 
